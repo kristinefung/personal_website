d0b28c9247cb1fbbb5f2f60a7d9f159bb1316b70
Sally Fung <sallyfung2018@gmail.com>
2025-07-20 20:39:42 +0800

    feat(login): implement Login component and page for user authentication with form handling and validation

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../../component/Header";
+import Login from "./Login";
+import Footer from "../../component/Footer";
+
+export default function LoginPage() {
+    return (
+        <>
+            <main className="bg-[#0a1628] min-h-screen">
+                <Login />
+            </main>
+        </>
+    );
+} 
